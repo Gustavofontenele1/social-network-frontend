b21697d12d1824b8c31c8dd0372c1816b91b59b5
Gustavofontenele1 <gustavofontenele@live.com>
2025-02-20 20:30:37 -0300

    Att

--- src/app/verify/[token]/page.tsx
+++ src/app/verify/[token]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import axios from "axios";
 
 const Verify: React.FC = () => {
@@ -10,6 +10,7 @@ const Verify: React.FC = () => {
   const [loading, setLoading] = useState(false);
 
   const { token } = useParams();
+  const router = useRouter();
 
   useEffect(() => {
     if (token && typeof token === "string") {
@@ -32,6 +33,9 @@ const Verify: React.FC = () => {
 
       if (req.status === 200) {
         setSuccessMessage("Conta verificada com sucesso! Você agora pode fazer login.");
+        setTimeout(() => {
+          router.push("/login");
+        }, 3000);
       } else {
         setErrorMessage("Erro ao verificar o token.");
       }
@@ -44,12 +48,16 @@ const Verify: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>Verificação de Conta</h1>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full sm:w-96 animate-fadeIn">
+        <h1 className="text-2xl font-semibold text-blue-600 mb-6">Verificação de Conta</h1>
 
-      {loading && <p>Verificando...</p>}
-      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-      {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
+        {loading && <p className="text-lg text-orange-500">Verificando...</p>}
+        {errorMessage && <p className="text-lg text-red-500 mt-4">{errorMessage}</p>}
+        {successMessage && (
+          <p className="text-lg text-green-500 mt-4">{successMessage}</p>
+        )}
+      </div>
     </div>
   );
 };
